63aed4a6b1423da763ae137bcdc42c66b3fc975c
Vanessa Villalba <vanessavvp2402@gmail.com>
2021-01-03 20:08:29 +0000

    Contando dedos, por corregir que aparezca el numero al tener levantado un solo dedo

--- practica3.py
+++ practica3.py
@@ -1,9 +1,11 @@
 import numpy as np
 import cv2 
 import math
+import imutils 
 
 cap = cv2.VideoCapture(0)
 backSub = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
+color_fingers = (0, 255, 255)
 
 if not cap.isOpened:
   print("Unable to open the cam")
@@ -49,16 +51,26 @@ while True:
       if (max < auxMax):
         max = auxMax
         index = i
-  
-    cv2.drawContours(roi, contours, index, (0,255,0))
-    #hull = cv2.convexHull(contours[index])
-    #cv2.drawContours(roi, [hull], 0, (255,0,0), 3)
 
+      #Encontrar centro
+      M = cv2.moments(cnt)
+      if M["m00"] == 0: M["m00"] = 1
+      x = int(M["m10"]/M["m00"])
+      y = int(M["m01"]/M["m00"])
+      cv2.circle(roi, tuple([x, y]), 5, (0, 255, 0), -1)
+
+      cv2.drawContours(roi, contours, index, (0,255,0))
+      #hull = cv2.convexHull(contours[index])
+      #cv2.drawContours(roi, [hull], 0, (255,0,0), 3)
     cnt = contours[index]
     hull = cv2.convexHull(cnt,returnPoints = False)
     defects = cv2.convexityDefects(cnt,hull)
 
     if defects is not None:
+      beginning = []
+      ending = []
+      fingers = 0
+       
       for i in range(len(defects)):
         s,e,f,d = defects[i,0]
         start = tuple(cnt[s][0])
@@ -67,13 +79,33 @@ while True:
         depth = d/256.0
         #print(depth)
         ang = angle(start,end,far)
-        cv2.line(roi,start,end,[255,0,0],2)
-        cv2.circle(roi,far,5,[0,0,255],-1)  
 
+        if np.linalg.norm(cnt[s][0] - cnt[e][0]) > 20 and d > 12000 and ang < 75:
+          beginning.append(start)
+          ending.append(end)
+          cv2.line(roi,start,end,[255,0,0],2)
+          cv2.circle(roi,far,5,[0,0,255],-1)  
+
+
+      #Cuando se levanta un solo dedo
+      if len(beginning) == 0:
+        minY = np.linalg.norm(cnt - [x, y])
+        if minY >= 850:
+          fingers = fingers + 1
+          #cv2.putText(roi, '{}'.format(fingers), (100,100), 1, 1, color_fingers, 1, cv2.LINE_AA)
+
+      for i in range(len(beginning)):
+        fingers = fingers + 1
+        cv2.putText(roi, '{}'.format(fingers), tuple(beginning[i]), 1, 1, color_fingers, 1, cv2.LINE_AA)
+        if i == len(beginning) - 1:
+          fingers = fingers + 1
+          cv2.putText(roi, '{}'.format(fingers), tuple(ending[i]), 1, 1, color_fingers, 1, cv2.LINE_AA)
+      cv2.putText(frame, '{}'.format(fingers), (390, 45), 1, 4, (color_fingers), 2, cv2.LINE_AA)
       rect = cv2.boundingRect(cnt)
       pt1_ = (rect[0],rect[1])
       pt2_ = (rect[0]+rect[2],rect[1]+rect[3])
       cv2.rectangle(roi,pt1_,pt2_,(0,0,255),3)
+
 
   cv2.imshow('FgMask', fgMask)
   cv2.imshow('frame', frame)
